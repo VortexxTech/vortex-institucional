ff1fcf371f34a07ca3ffd7b82f013731881a69f6
matheuscastro <matheus.csilva@sptech.school>
2024-11-12 13:37:20 -0300

    ajuste nos inputs de pesquisa

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,71 @@
+const inputZona = document.querySelector("#select_zone");
+const inputBairro = document.querySelector("#select_neighbourhood");
+const inputComercio = document.querySelector("#select_business");
+const btnCheckRegion = document.querySelector("#btn-check-region");
+
+inputZona.addEventListener("change", event => showOptions(event.target.value));
+
+const bairrosLeste = [
+    { value: "tatuape", option: "Tatuapé" },
+    { value: "analia-franco", option: "Anália Franco" },
+    { value: "mooca", option: "Mooca" },
+    { value: "vila-prudente", option: "Vila Prudente" },
+    { value: "vila-carrao", option: "Vila Carrão" }
+];
+
+const bairrosOeste = [
+    { value: "jardins", option: "Jardins" },
+    { value: "pinheiros", option: "Pinheiros" },
+    { value: "vila-madalena", option: "Vila Madalena" },
+    { value: "perdizes", option: "Perdizes" },
+    { value: "pacaembu", option: "Pacaembu" }
+];
+
+const bairrosNorte = [
+    { value: "santana", option: "Santana" },
+    { value: "tucuruvi", option: "Tucuruvi" },
+    { value: "agua-fria", option: "Água Fria" },
+    { value: "mandaqui", option: "Mandaqui" },
+    { value: "jardim-franca", option: "Jardim França" }
+];
+
+const bairrosSul = [
+    { value: "itaim-bibi", option: "Itaim Bibi" },
+    { value: "moema", option: "Moema" },
+    { value: "campo-belo", option: "Campo Belo" },
+    { value: "brooklin", option: "Brooklin" },
+    { value: "chacara-flora", option: "Chácara Flora" }
+];
+
+function createListOptions(list) {
+    return list.map(bairro => `<option value="${bairro.value}">${bairro.option}</option>`);
+}
+
+function showOptions(zona) {
+    console.log(zona)
+    if(zona === "leste") {
+        inputBairro.innerHTML = `
+            <option value="#">Escolha um bairro</option>
+            ${createListOptions(bairrosLeste)}
+        `;
+    } else if(zona === "oeste") {
+        inputBairro.innerHTML = `
+            <option value="#">Escolha um bairro</option>
+            ${createListOptions(bairrosOeste)}
+        `;
+    } else if(zona === "norte") {
+        inputBairro.innerHTML = `
+            <option value="#">Escolha um bairro</option>
+            ${createListOptions(bairrosNorte)}
+        `;
+    } else {
+        inputBairro.innerHTML = `
+            <option value="#">Escolha um bairro</option>
+            ${createListOptions(bairrosSul)}
+        `;
+    }
+}
+
 function redirecionar() {
     window.location.href = "./login.html";
 }
@@ -5,31 +73,3 @@ function redirecionar() {
 function logout() {
     sessionStorage.clear();
 }
-
-// Initialize and add the map
-let map;
-async function initMap() {
-
-    // The location of Uluru
-    const position = { lat: -23.557577, lng: -46.686329 };
-    // Request needed libraries.
-    //@ts-ignore
-    const { Map } = await google.maps.importLibrary("maps");
-    const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
-
-    // The map, centered at Uluru
-    map = new Map(document.getElementById("google-maps"), {
-    zoom: 15,
-    center: position,
-    mapId: "testeing",
-    });
-
-    // The marker, positioned at Uluru
-    const marker = new AdvancedMarkerElement({
-    map: map,
-    position: position,
-    title: "Uluru",
-    });
-}
-
-initMap();
