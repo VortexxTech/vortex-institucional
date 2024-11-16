fcd9e0b10583082f4f99a9ded8661c4666bfa825
RafaellaGuedes <rafaella.macedo@sptech.school>
2024-11-16 14:59:55 -0300

    Adicionando plotagem nos graficos

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -155,9 +155,450 @@ function aparecer_previsao() {
 
 }
 
+function obterRendaPerCapta() {
+    fetch(`/rendaPerCapta/buscarRendaPerCapta`, { cache: 'no-store' }).then(function (response) {
+        if (response.ok) {
+            response.json().then(function (resposta) {
+                console.log(`Dados recebidos: ${JSON.stringify(resposta)}`);
+                resposta.reverse();
 
+                plotarRendaPerCapta(resposta);
+            });
+        } else {
+            console.error('Nenhum dado encontrado ou erro na API');
+        }
+    })
+        .catch(function (error) {
+            console.error(`Erro na obtenção dos dados p/ gráfico: ${error.message}`);
+        });
+}
 
+function plotarRendaPerCapta(resposta) {
 
+    console.log('iniciando plotagem do gráfico...');
+
+    let labels = [];
+
+    let borderColors = [];
+
+    let dados = {
+        labels: labels,
+        datasets: [{
+            label: 'Quantidade de Usuários',
+            data: [],
+            fill: false,
+            borderColor: borderColors,
+            backgroundColor: borderColors,
+            tension: 0.1
+        }]
+    };
+
+    const colorPalette = [
+        'rgb(255, 99, 132)', 
+        'rgb(54, 162, 235)', 
+        'rgb(255, 206, 86)', 
+        'rgb(75, 192, 192)', 
+        'rgb(153, 102, 255)', 
+        'rgb(255, 159, 64)',
+        'rgb(0, 255, 127)', 
+        'rgb(255, 0, 255)'
+    ];
+
+    console.log('----------------------------------------------')
+    console.log('Estes dados foram recebidos pela funcao "obterDadosGrafico" e passados para "plotarGrafico":')
+    console.log(resposta)
+
+    for (var i = 0; i < resposta.length; i++) {
+        var registro = resposta[i];
+        labels.push(registro.religiao);
+        dados.datasets[0].data.push(registro.quantidade);
+
+        let colorIndex = i % colorPalette.length;
+        borderColors.push(colorPalette[colorIndex]);
+    }
+
+    console.log('----------------------------------------------')
+    console.log('O gráfico será plotado com os respectivos valores:')
+    console.log('Labels:')
+    console.log(labels)
+    console.log('Dados:')
+    console.log(dados.datasets)
+    console.log('----------------------------------------------')
+
+    const config = {
+        type: 'bar',
+        data: dados,
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    };
+    
+    let myChart2 = new Chart(
+        document.getElementById(`religion_canvas`),
+        config
+    );
+
+}
+
+function obterPrecoM2() {
+    fetch(`/precoM2/buscarPreco`, { cache: 'no-store' }).then(function (response) {
+        if (response.ok) {
+            response.json().then(function (resposta) {
+                console.log(`Dados recebidos: ${JSON.stringify(resposta)}`);
+                resposta.reverse();
+
+                plotarPrecoM2(resposta);
+            });
+        } else {
+            console.error('Nenhum dado encontrado ou erro na API');
+        }
+    })
+        .catch(function (error) {
+            console.error(`Erro na obtenção dos dados p/ gráfico: ${error.message}`);
+        });
+}
+
+function plotarPrecoM2(resposta) {
+
+    console.log('iniciando plotagem do gráfico...');
+
+    let labels = [];
+
+    let borderColors = [];
+
+    let dados = {
+        labels: labels,
+        datasets: [{
+            label: 'Quantidade de Usuários',
+            data: [],
+            fill: false,
+            borderColor: borderColors,
+            backgroundColor: borderColors,
+            tension: 0.1
+        }]
+    };
+
+    const colorPalette = [
+        'rgb(255, 99, 132)', 
+        'rgb(54, 162, 235)', 
+        'rgb(255, 206, 86)', 
+        'rgb(75, 192, 192)', 
+        'rgb(153, 102, 255)', 
+        'rgb(255, 159, 64)',
+        'rgb(0, 255, 127)', 
+        'rgb(255, 0, 255)'
+    ];
+
+    function obterRendaPerCapta() {
+    fetch(`/rendaPerCapta/buscarRendaPerCapta`, { cache: 'no-store' }).then(function (response) {
+        if (response.ok) {
+            response.json().then(function (resposta) {
+                console.log(`Dados recebidos: ${JSON.stringify(resposta)}`);
+                resposta.reverse();
+
+                plotarRendaPerCapta(resposta);
+            });
+        } else {
+            console.error('Nenhum dado encontrado ou erro na API');
+        }
+    })
+        .catch(function (error) {
+            console.error(`Erro na obtenção dos dados p/ gráfico: ${error.message}`);
+        });
+}
+
+function plotarRendaPerCapta(resposta) {
+
+    console.log('iniciando plotagem do gráfico...');
+
+    let labels = [];
+
+    let borderColors = [];
+
+    let dados = {
+        labels: labels,
+        datasets: [{
+            label: 'Quantidade de Usuários',
+            data: [],
+            fill: false,
+            borderColor: borderColors,
+            backgroundColor: borderColors,
+            tension: 0.1
+        }]
+    };
+
+    const colorPalette = [
+        'rgb(255, 99, 132)', 
+        'rgb(54, 162, 235)', 
+        'rgb(255, 206, 86)', 
+        'rgb(75, 192, 192)', 
+        'rgb(153, 102, 255)', 
+        'rgb(255, 159, 64)',
+        'rgb(0, 255, 127)', 
+        'rgb(255, 0, 255)'
+    ];
+
+    console.log('----------------------------------------------')
+    console.log('Estes dados foram recebidos pela funcao "obterDadosGrafico" e passados para "plotarGrafico":')
+    console.log(resposta)
+
+    for (var i = 0; i < resposta.length; i++) {
+        var registro = resposta[i];
+        labels.push(registro.religiao);
+        dados.datasets[0].data.push(registro.quantidade);
+
+        let colorIndex = i % colorPalette.length;
+        borderColors.push(colorPalette[colorIndex]);
+    }
+
+    console.log('----------------------------------------------')
+    console.log('O gráfico será plotado com os respectivos valores:')
+    console.log('Labels:')
+    console.log(labels)
+    console.log('Dados:')
+    console.log(dados.datasets)
+    console.log('----------------------------------------------')
+
+    const config = {
+        type: 'bar',
+        data: dados,
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    };
+    
+    let myChart2 = new Chart(
+        document.getElementById(`religion_canvas`),
+        config
+    );
+
+}
+
+    console.log('----------------------------------------------')
+    console.log('Estes dados foram recebidos pela funcao "obterDadosGrafico" e passados para "plotarGrafico":')
+    console.log(resposta)
+
+    for (var i = 0; i < resposta.length; i++) {
+        var registro = resposta[i];
+        labels.push(registro.religiao);
+        dados.datasets[0].data.push(registro.quantidade);
+
+        let colorIndex = i % colorPalette.length;
+        borderColors.push(colorPalette[colorIndex]);
+    }
+
+    console.log('----------------------------------------------')
+    console.log('O gráfico será plotado com os respectivos valores:')
+    console.log('Labels:')
+    console.log(labels)
+    console.log('Dados:')
+    console.log(dados.datasets)
+    console.log('----------------------------------------------')
+
+    const config = {
+        type: 'bar',
+        data: dados,
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    };
+    
+    let myChart2 = new Chart(
+        document.getElementById(`religion_canvas`),
+        config
+    );
+
+}
+
+function obterIdh() {
+    fetch(`/idh/buscarIdh`, { cache: 'no-store' }).then(function (response) {
+        if (response.ok) {
+            response.json().then(function (resposta) {
+                console.log(`Dados recebidos: ${JSON.stringify(resposta)}`);
+                resposta.reverse();
+
+                plotarIdh(resposta);
+            });
+        } else {
+            console.error('Nenhum dado encontrado ou erro na API');
+        }
+    })
+        .catch(function (error) {
+            console.error(`Erro na obtenção dos dados p/ gráfico: ${error.message}`);
+        });
+}
+
+function plotarIdh(resposta) {
+
+    console.log('iniciando plotagem do gráfico...');
+
+    let labels = [];
+
+    let borderColors = [];
+
+    let dados = {
+        labels: labels,
+        datasets: [{
+            label: 'Quantidade de Usuários',
+            data: [],
+            fill: false,
+            borderColor: borderColors,
+            backgroundColor: borderColors,
+            tension: 0.1
+        }]
+    };
+
+    const colorPalette = [
+        'rgb(255, 99, 132)', 
+        'rgb(54, 162, 235)', 
+        'rgb(255, 206, 86)', 
+        'rgb(75, 192, 192)', 
+        'rgb(153, 102, 255)', 
+        'rgb(255, 159, 64)',
+        'rgb(0, 255, 127)', 
+        'rgb(255, 0, 255)'
+    ];
+
+    console.log('----------------------------------------------')
+    console.log('Estes dados foram recebidos pela funcao "obterDadosGrafico" e passados para "plotarGrafico":')
+    console.log(resposta)
+
+    for (var i = 0; i < resposta.length; i++) {
+        var registro = resposta[i];
+        labels.push(registro.religiao);
+        dados.datasets[0].data.push(registro.quantidade);
+
+        let colorIndex = i % colorPalette.length;
+        borderColors.push(colorPalette[colorIndex]);
+    }
+
+    console.log('----------------------------------------------')
+    console.log('O gráfico será plotado com os respectivos valores:')
+    console.log('Labels:')
+    console.log(labels)
+    console.log('Dados:')
+    console.log(dados.datasets)
+    console.log('----------------------------------------------')
+
+    const config = {
+        type: 'bar',
+        data: dados,
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    };
+    
+    let myChart2 = new Chart(
+        document.getElementById(`religion_canvas`),
+        config
+    );
+
+}
+
+function obterDensidadeDemografica() {
+    fetch(`/densidadeDemografica/buscarDensidadeDemografica`, { cache: 'no-store' }).then(function (response) {
+        if (response.ok) {
+            response.json().then(function (resposta) {
+                console.log(`Dados recebidos: ${JSON.stringify(resposta)}`);
+                resposta.reverse();
+
+                plotarDensidadeDemografica(resposta);
+            });
+        } else {
+            console.error('Nenhum dado encontrado ou erro na API');
+        }
+    })
+        .catch(function (error) {
+            console.error(`Erro na obtenção dos dados p/ gráfico: ${error.message}`);
+        });
+}
+
+function plotarDensidadeDemografica(resposta) {
+
+    console.log('iniciando plotagem do gráfico...');
+
+    let labels = [];
+
+    let borderColors = [];
+
+    let dados = {
+        labels: labels,
+        datasets: [{
+            label: 'Quantidade de Usuários',
+            data: [],
+            fill: false,
+            borderColor: borderColors,
+            backgroundColor: borderColors,
+            tension: 0.1
+        }]
+    };
+
+    const colorPalette = [
+        'rgb(255, 99, 132)', 
+        'rgb(54, 162, 235)', 
+        'rgb(255, 206, 86)', 
+        'rgb(75, 192, 192)', 
+        'rgb(153, 102, 255)', 
+        'rgb(255, 159, 64)',
+        'rgb(0, 255, 127)', 
+        'rgb(255, 0, 255)'
+    ];
+
+    console.log('----------------------------------------------')
+    console.log('Estes dados foram recebidos pela funcao "obterDadosGrafico" e passados para "plotarGrafico":')
+    console.log(resposta)
+
+    for (var i = 0; i < resposta.length; i++) {
+        var registro = resposta[i];
+        labels.push(registro.religiao);
+        dados.datasets[0].data.push(registro.quantidade);
+
+        let colorIndex = i % colorPalette.length;
+        borderColors.push(colorPalette[colorIndex]);
+    }
+
+    console.log('----------------------------------------------')
+    console.log('O gráfico será plotado com os respectivos valores:')
+    console.log('Labels:')
+    console.log(labels)
+    console.log('Dados:')
+    console.log(dados.datasets)
+    console.log('----------------------------------------------')
+
+    const config = {
+        type: 'bar',
+        data: dados,
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    };
+    
+    let myChart2 = new Chart(
+        document.getElementById(`religion_canvas`),
+        config
+    );
+
+}
 
 const ctx = document.getElementById('grafico');
 const grafico = new Chart(ctx, {
