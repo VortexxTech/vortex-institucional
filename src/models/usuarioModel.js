dc64c497e91a46b764f07ae708db9da9989a2b2c
Felipe Janazi <160364132+Felipe-Janazi@users.noreply.github.com>
2024-11-29 21:05:40 -0300

    Update usuarioModel.js

--- src/models/usuarioModel.js
+++ src/models/usuarioModel.js
@@ -3,7 +3,7 @@ var database = require("../database/config")
 function autenticar(email, senha) {
     console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function entrar(): ", email, senha)
     var instrucaoSql = `
-        SELECT id_usuario, nome, senha FROM Usuario WHERE email = '${email}' AND senha = '${senha}';
+        SELECT idUsuario, nome, senha FROM Usuario WHERE email = '${email}' AND senha = '${senha}';
     `;
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
@@ -13,7 +13,7 @@ function cadastrar(nome, email, senha) {
     console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():", nome, email, senha);
 
     var instrucaoSql = `
-        INSERT INTO Usuario (id_usuario, nome, email, senha) VALUES (default, '${nome}', '${email}', '${senha}');
+        INSERT INTO Usuario (idUsuario, nome, email, senha) VALUES (default, '${nome}', '${email}', '${senha}');
     `;
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
